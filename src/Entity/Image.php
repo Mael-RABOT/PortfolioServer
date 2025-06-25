6ced0e37ed5422ea57d1daffc14285c4de84bde5
Mael-RABOT <mael.rabot@epitech.eu>
2025-06-25 12:30:57 +0200

    evol(api): allow put on item, image and link
    Signed-off-by: Mael-RABOT <mael.rabot@epitech.eu>

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -3,11 +3,26 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
 use App\Repository\ImageRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ImageRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Patch(),
+        new Delete()
+    ]
+)]
 class Image
 {
     #[ORM\Id]
